3bc9ad6484703319a9aabca971dbc30dd304e5d7
Varsha Jacob <55629256+VJac15@users.noreply.github.com>
2022-01-16 19:08:50 -0500

    Workshop Carousel

--- pages/workshops.jsx
+++ pages/workshops.jsx
@@ -1,14 +1,51 @@
 import React from 'react';
 
-import Content from '../components/Content';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 export default function Index() {
 	return (
 		<>
-		<div class = "container">
-			<h2 class="head-title">Upcoming Workshops + Past Events!</h2>
-			<br />
-		</div>
+		 <div class="container-fluid">
+  <div class = "row">
+    <div class = "col-md-8 col-sm-12 col-xs-12 competition-header">
+      <h1 class = "header">Workshops</h1>
+    </div>
+  </div>
+</div>
+		
+		<div>
+			
+              <Carousel showThumbs ={false}>
+			  <div>
+				  <iframe width="560" height="650" src="https://www.youtube.com/embed/videoseries?list=PLBIRH14k-h4W6xuNoLpZ9pna2HlEPXRIV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                      <p className="legend">Keynotes</p>
+  
+                  </div>
+			  <div>
+				  <iframe width="560" height="650" src="https://www.youtube.com/embed/7u2lHzf89xg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                      <p className="legend">10-Step Design Process</p>
+  
+                  </div>
+                  
+				  <div class = "carousel-component">
+				  <iframe width="560" height="650" src="https://www.youtube.com/embed/videoseries?list=PLBIRH14k-h4UYjgleMe9kLmgEYxGcnJqk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				  <p className="legend">Web Development Series</p>
+  
+                  </div>
+                  <div>
+				  <iframe width="560" height="650" src="https://www.youtube.com/embed/videoseries?list=PLBIRH14k-h4Vqkad0rY_j-s3D_Ybf_Ls2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                      <p className="legend">Data Science Series</p>
+  
+                  </div>
+                  <div>
+				  <iframe width="560" height="650" src="https://www.youtube.com/embed/videoseries?list=PLBIRH14k-h4XiaLDMGLzH1YGBTAD1NNtj" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                      <p className="legend">Android App Development Series</p>
+  
+                  </div>
+                  
+              </Carousel>
+            </div>
 
 		</>
 		);
